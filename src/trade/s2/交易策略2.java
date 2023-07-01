54c5bbe29d4f8e2cb2cdb41802e0b18612c5451e
周游 <mickey_zhouyou@yeah.net>
2023-07-01 11:58:06 +0800

    a

--- src/trade/s2/交易策略2.java
+++ src/trade/s2/交易策略2.java
@@ -1,5 +1,8 @@
 package trade.s2;
 
+import t.OptionDate;
+import trade.Account;
+
 /**
  * 1. 以买出跨式期权为主开仓
  * 2. 每天检查期权标的现价是否接近或者跨越一级（0.05），如果有，则调整虚值期权到平值
@@ -7,4 +10,14 @@ package trade.s2;
  * 4. 在调整合约到平值的时候下月同一行权价的合约的权利金是否达到当月合约一倍，如果有，则卖下个月的，否则卖本月的。
  */
 public class 交易策略2 {
+
+    private Account account;
+
+    public 交易策略2(Account account) {
+        this.account = account;
+    }
+
+    public void 交易(OptionDate today) {
+        
+    }
 }
